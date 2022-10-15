0278b5dcb15bbffe71913dbc178c1ac33099dc2b
Collin Jackson <collinjackson@fastmail.com>
2022-10-15 13:14:24 -0500

    Use schedule module for lights

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import sys
 from datetime import datetime, time
 from time import sleep
+import schedule
 from src.outlet import Outlet
 
 ON_TIME = time(7, 00)
 OFF_TIME = time(18, 00)
 
+def should_be_on() -> bool:
+    return ON_TIME <= datetime.now().time() <= OFF_TIME
+
 def main() -> int:
     with Outlet() as outlet:
+        if should_be_on():
+            outlet.turn_on()
+
+        schedule.every().day.at(ON_TIME.isoformat()).do(outlet.turn_on)
+        schedule.every().day.at(OFF_TIME.isoformat()).do(outlet.turn_off)
         while True:
-            CUR_TIME = datetime.now().time()
-            if (ON_TIME <= CUR_TIME <= OFF_TIME) and not outlet.is_on:
-                outlet.turn_on()
-            elif (CUR_TIME > OFF_TIME or CUR_TIME < ON_TIME) and outlet.is_on:
-                outlet.turn_off()
-            sleep(30)
+            schedule.run_pending()
+            sleep(1)
     return 0
 
 if __name__ == '__main__':
