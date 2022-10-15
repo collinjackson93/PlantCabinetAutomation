e1139667c4820564e5e27a641c136e47ef43f667
Collin Jackson <collinjackson@fastmail.com>
2022-10-15 14:53:36 -0500

    Add Grafana metrics for temp and humidity

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ RaspberryPi automation for a greenhouse cabinet with an [AHT20 temperature/humid
 * Install CircuitPython/Blinka [link](https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi#update-your-pi-and-python-2993452)
 * Install this python module
 * Connect the humidity sensor to the I2C pins and the IOT relay to a GPIO (pin 17 by default)
+* Install Grafana agent [link](https://grafana.com/docs/agent/latest/set-up/install-agent-linux/)
+* Create Hosted Prometheus API key and configuration in Grafana cloud
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import sys
 from datetime import datetime, time
 from time import sleep
 import schedule
+from prometheus_client import start_http_server, Gauge
 from src.outlet import Outlet
+from src.sensor import Sensor
 
 ON_TIME = time(7, 00)
 OFF_TIME = time(18, 00)
@@ -15,8 +17,25 @@ def main() -> int:
         if should_be_on():
             outlet.turn_on()
 
+        sensor = Sensor()
+        start_http_server(9100) # port defined by Grafana agent config
+        humidity = Gauge('cabinet_humidity', 'Relative humidity')
+        temp = Gauge('cabinet_temp_fahrenheit', 'Temperature in F')
+
+        # Schedule sensor readings
+        check_humidity = lambda : humidity.set(sensor.humidity)
+        schedule.every(5).minutes.do(check_humidity)
+        check_temp = lambda : temp.set(sensor.temperature)
+        schedule.every(5).minutes.do(check_temp)
+
+        # Take initial readings
+        check_humidity()
+        check_temp()
+
+        # Schedule lights
         schedule.every().day.at(ON_TIME.isoformat()).do(outlet.turn_on)
         schedule.every().day.at(OFF_TIME.isoformat()).do(outlet.turn_off)
+
         while True:
             schedule.run_pending()
             sleep(1)
